df58826ab757c09e0e2545a169fa8a949cd26966
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-09 16:50:36 +0800

    3. Tate Collection Metadata: Take Two

---
+++ demo_3.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+import pandas as pd
+
+# Example usage of from_records method
+records = [("Espresso", "5$"),
+           ("Flat White", "10$")]
+
+df = pd.DataFrame.from_records(records)
+print(df)
+
+df = pd.DataFrame.from_records(records, columns=["Coffee", "Price"])
+print(df)
+
+####
+KEYS_TO_USE = ['id', 'all_artists', 'title', 'medium', 'acquisitionYear', 'height', 'width']
+
+
+def get_record_from_file(file_path, keys_to_use):
+    """ Process single json file and return a tuple containing specific fields."""
+
+    with open(file_path) as artwork_file:
+        content = json.load(artwork_file)
+
+    record = []
+    for field in keys_to_use:
+        record.append(content[field])
+
+    return tuple(record)
+
+# Single file processing function demo
+SAMPLE_JSON = os.path.join('/Users/kmandawe/data/pandas-fundamentals/', 'artworks', 'a', '000', 'a00001-1035.json')
+
+sample_record = get_record_from_file(SAMPLE_JSON, KEYS_TO_USE)
+print(sample_record)
+
+def read_artworks_from_json(keys_to_use):
+    """ Traverse the directories with JSON files.
+    For first file in each directory call function
+    for processing single file and go to the next
+    directory.
+    """
+    JSON_ROOT = os.path.join('/Users/kmandawe/data/pandas-fundamentals/', 'artworks')
+
+    artworks = []
+    for root, _, files in os.walk(JSON_ROOT):
+        for f in files:
+            if f.endswith('.json'):
+                record = get_record_from_file(os.path.join(root, f), keys_to_use)
+                artworks.append(record)
+            break
+
+    df = pd.DataFrame.from_records(artworks, columns=keys_to_use, index='id'    )
+    return df
+
+
+df = read_artworks_from_json(KEYS_TO_USE)
+print(df)
